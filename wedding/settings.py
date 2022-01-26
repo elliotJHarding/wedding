9706c2f99a09d24f68b228ff339ebf44b9830bd4
Elliot Harding <elliot@hardings.org.uk>
2022-01-26 23:16:34 +0000

    Fixed issue with exposed secret key

--- wedding/settings.py
+++ wedding/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import socket
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v()d&g6*j^9t+1v6yo()uq_f7*!ukhw=*@+tim1s$on-p8kstj'
+
+SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.20", "elliot-linux"]
+ALLOWED_HOSTS = ["elliot-linux", socket.gethostbyname(socket.gethostname())]
 
 
 # Application definition
