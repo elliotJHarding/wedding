59466397b13b3a727e6424bf59f1953549a13c8c
Elliot Harding <elliot@hardings.org.uk>
2022-01-31 22:30:43 +0000

    db fixes in settings.py

--- wedding/settings.py
+++ wedding/settings.py
@@ -83,14 +83,11 @@ WSGI_APPLICATION = 'wedding.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = db_from_env
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -184,6 +181,7 @@ LOGGING = {
 
 django_heroku.settings(locals(), logging=False)
 
-
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmode', None)
 
 
