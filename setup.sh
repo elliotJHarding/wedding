cb0e73539ba9ab929d661c15a4459d32ece41647
Elliot Harding <52636757+elliotJHarding@users.noreply.github.com>
2022-02-11 13:46:11 +0000

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export SECRET_KEY='django-insecure-v()d&g6*j^9t+1v6yo()uq_f7*!ukhw=*@+tim1s$on-p8kstj'
-export DATABASE_URL='postgres://gblxcbxfatgvxp:4c79158a420dc23f59b8a6a278af11084baf3852a1e8bcd0e98a704b7f28b7c9@ec2-3-216-113-109.compute-1.amazonaws.com:5432/da54qoi4et1j32'
-export SSL_REDIRECT=0
-export DEBUG=1
-export EMAIL_HOST='email-smtp.eu-west-2.amazonaws.com'
-export EMAIL_HOST_USER='AKIARDNIKJDPYB7GV3EJ'
-export EMAIL_HOST_PASSWORD='BE8uxqE9Vm1jPgpgEkcxbJBtQF9u0r0DY27hqMb7GIUK'
